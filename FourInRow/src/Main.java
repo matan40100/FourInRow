1a92dec68cd4a93c4237a1d22902d874eda4da29
matan40100 <49817274+matan40100@users.noreply.github.com>
2020-01-20 19:31:33 +0200

    .

--- FourInRow/src/Main.java
+++ FourInRow/src/Main.java
@@ -1,12 +1,19 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ImageIcon icon = new ImageIcon("icon.png");
-		String[] options = { "Normal game", "Human Vs Computer", "Custom game" };
+		String[] options = { "Normal game", "Human Vs Computer", "Custom game", "Create local game", "Connect to server" };
 
 		int response = JOptionPane.showOptionDialog(null, "Choose game type:", "Game Settings",
 				JOptionPane.PLAIN_MESSAGE, JOptionPane.INFORMATION_MESSAGE, icon, options, options[0]);
@@ -26,12 +33,37 @@ public class Main {
 		case 2:
 			createCustomGame();
 			break;
+		case 3:
+			createLocalGame();
+			break;
 
 		default:
 			break;
 		}
 
 	}
+	
+	public static void createLocalGame() {
+		boolean connect = false;
+		try {
+			String ip = String.valueOf(Inet4Address.getLocalHost());
+			ip = ip.substring(ip.indexOf("/")+1);
+			
+			JOptionPane.showMessageDialog(null,"find \n d"
+					);
+			ServerSocket serverSocket = new  ServerSocket(9999);
+			Socket socket = serverSocket.accept();
+			
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	        
+
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			System.err.println("sas");
+		}
+	}
+	
 
 	public static void createCustomGame() {
 		JTextField numOfRowfField = new JTextField();
